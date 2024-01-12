49517e7d4811e01d880cdbba3a351907993c2ad1
yejian <yejian@zhito.com>
2024-01-12 16:18:06 +0800

    add cmake project hello-world version 2

---
+++ Note_2/hello-world/include/message.h
@@ -0,0 +1,13 @@
+#ifndef MESSAGE_HEADER_H_
+#define MESSAGE_HEADER_H_
+
+#include <iostream>
+#include <string>
+
+class Message {
+ public:
+  Message() {}
+
+  void Print(const std::string &message);
+};
+#endif // ! MESSAGE_HEADER_H_
---
+++ Note_2/hello-world/src/message.cpp
@@ -0,0 +1,6 @@
+
+#include "message.h"
+
+void Message::Print(const std::string &message) {
+  std::cout << message << std::endl;
+}
